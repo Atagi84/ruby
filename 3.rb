338afa0c3a2b1592e76f4bf0421353f0ef5302b4
Atagi84 <53830854+Atagi84@users.noreply.github.com>
2019-08-18 14:21:18 +0200

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -1,23 +1,4 @@
-puts 'Hello world'
-
-Zadani 1
-
-puts "Napis priklad:"
-
-number_1 = gets.to_i
-number_2 = gets.to_i
-
-number_3 = number_1 + number_2
-
-if number_1 == number_2
-  puts number_3 * 3
-else
-  puts number_3
-end
-
-Zadani 3
-
- puts "Napis priklad"
+puts "Napis priklad"
 integer_1 = gets.to_i
 integer_2 = gets.to_i
 
