af8c6640ba2f66536c8b33e33e80316270ef25be
Atagi84 <53830854+Atagi84@users.noreply.github.com>
2019-08-18 14:16:06 +0200

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -1,1 +1,28 @@
 puts 'Hello world'
+
+Zadani 1
+
+puts "Napis priklad:"
+
+number_1 = gets.to_i
+number_2 = gets.to_i
+
+number_3 = number_1 + number_2
+
+if number_1 == number_2
+  puts number_3 * 3
+else
+  puts number_3
+end
+
+Zadani 3
+
+ puts "Napis priklad"
+integer_1 = gets.to_i
+integer_2 = gets.to_i
+
+if integer_1 == 30 || integer_2 == 30 || integer_1 + integer_2 == 30
+  puts true
+else
+  puts false
+end
